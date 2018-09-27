4c53109bfab84ef92d5ce5145e988d5d5f64aac7
vdowsett <vdowsett@gmail.com>
2018-09-27 11:23:10 -0500

    adding play pause ability to Album.js

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,8 +10,42 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+
+    }
+    
+    play() {
+      this.audioElement.play();
+      this.setState({ isPlaying: true });
+    }
+
+    pause() {
+      this.audioElement.pause();
+      this.setState({ isPlaying: false });
+    }
+
+    setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+    }
+
+    handleSongClick(song) {
+      const isSameSong = this.state.currentSong === song;
+      if (this.state.isPlaying && isSameSong) {
+        this.pause();
+      } else {
+        if (!isSameSong) { this.setSong(song); }
+        this.play();
+      }
+
+
+
 
   }
 
@@ -41,7 +75,7 @@ class Album extends Component {
 
              {
                this.state.album.songs.map( ( song, index ) =>
-                 <tr className="song" key={index}>
+                 <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
                    <td className="song-table-details"> {index+1} </td>
                    <td className="song-table-details"> {song.title} </td>
                    <td className="song-table-details"> {song.duration} </td>
