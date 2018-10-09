8b3c43ca773ca98db87526bc2632cd926d498cd9
vdowsett <vdowsett@gmail.com>
2018-10-09 12:54:47 -0500

    time and time slider functionality

--- src/components/Album.js
+++ src/components/Album.js
@@ -25,13 +25,24 @@ class Album extends Component {
     }
 
     componentDidMount() {
-      this.audioElement.addEventListener('timeupdate', (e) => {
-       this.setState({ currentTime: this.audioElement.currentTime });
-     });
-     this.audioElement.addEventListener('durationchange', (e) => {
-       this.setState({ duration: this.audioElement.duration });
-     });
-   }
+      this.eventListeners = {
+        timeupdate: e => {
+          this.setState({ currentTime: this.audioElement.currentTime });
+        },
+        durationchange: e => {
+          this.setState({ duration: this.audioElement.duration });
+        }
+      };
+      this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+      this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+    }
+
+    componentWillUnmount() {
+      this.audioElement.src = null;
+      this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+      this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+    }
+
 
     play() {
       this.audioElement.play();
@@ -81,6 +92,13 @@ class Album extends Component {
     handleMouseLeave(index) {
       this.setState({ isHovered: false });
     }
+
+    handleTimeChange(e) {
+    const newTime = this.audioElement.duration * e.target.value;
+    this.audioElement.currentTime = newTime;
+    this.setState({ currentTime: newTime });
+    }
+
 
   render() {
     return (
@@ -143,6 +161,7 @@ class Album extends Component {
            handleSongClick={ () => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={ () => this.handlePrevClick()}
            handleNextClick={ () => this.handleNextClick()}
+           handleTimeChange={ (e) => this.handleTimeChange(e)}   
            />
 
       </section>
